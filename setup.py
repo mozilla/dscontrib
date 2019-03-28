d0b41bf6c3c7f9c49cd6463c80b797fef8f88077
Rob Hudson <robhudson@mozilla.com>
2019-03-28 16:46:00 -0700

    Deploy to PyPI on every master commit
    Fixes #6

--- setup.py
+++ setup.py
@@ -4,27 +4,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from setuptools import setup, find_packages
+import datetime
+
+from setuptools import find_packages, setup
 
 
 setup(
-    name='dscontrib',
-    use_scm_version=True,
-    author='Rob Hudson',
-    author_email='robhudson@mozilla.com',
-    description='A Python library for Mozilla Data Science code snippets',
-    url='https://github.com/mozilla/dscontrib',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="dscontrib",
+    version=datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
+    author="Rob Hudson",
+    author_email="robhudson@mozilla.com",
+    description="A Python library for Mozilla Data Science code snippets",
+    url="https://github.com/mozilla/dscontrib",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
-        'numpy',
-        'pandas',
-        'py4j',
-        'pyarrow>=0.8.0',
-        'pyspark',
-        'scipy',
+        "numpy",
+        "pandas",
+        "py4j",
+        "pyarrow>=0.8.0",
+        "pyspark",
+        "scipy"
     ],
-    setup_requires=[
-        'setuptools-scm',
-    ]
 )

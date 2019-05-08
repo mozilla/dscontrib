cc915d2406533f765db75ebd24e8867f4a3f8cd2
benmiroglio <bmiroglio@mozilla.com>
2019-05-08 09:04:46 -0700

    Fix flake8 errors

--- src/dscontrib/sustainability/utils.py
+++ src/dscontrib/sustainability/utils.py
@@ -2,27 +2,31 @@ import pandas as pd
 import pyspark.sql.functions as F
 
 
-def bootstrap(df, iterations, reporting_freq=10, strata=[], 
+def bootstrap(df, iterations, reporting_freq=10, strata=[],
               treatment='variant', control='control'):
-  def munge(s):
-    agg = s.groupby(["experiment_branch"] + strata).mean()
-    agg = pd.melt(agg.reset_index(), id_vars=["experiment_branch"] + strata).pivot_table(
-                index=strata + ['variable'],
-                columns='experiment_branch',
-                values='value').reset_index()
-    agg['change'] = (agg[treatment] - agg[control]) / agg[control]
-    return agg
-    
-  n = len(df)
-  results = munge(df)
-  results['iteration'] = -1 # true sample stats lie at -1
-  for i in range(iterations):
-    if i % 10 == 0: print i,
-    s = df.sample(n, replace=True)
-    sagg = munge(s)
-    sagg['iteration'] = i
-    results = pd.concat([sagg, results])
-  return results
+    def munge(s):
+        agg = s.groupby(["experiment_branch"] + strata).mean()
+        agg = (
+          pd.melt(agg.reset_index(), id_vars=["experiment_branch"] + strata)
+          .pivot_table(
+                    index=strata + ['variable'],
+                    columns='experiment_branch',
+                    values='value').reset_index()
+          )
+        agg['change'] = (agg[treatment] - agg[control]) / agg[control]
+        return agg
+
+    n = len(df)
+    results = munge(df)
+    results['iteration'] = -1  # true sample stats lie at -1
+    for i in range(iterations):
+        if i % 10 == 0:
+            print i,
+        s = df.sample(n, replace=True)
+        sagg = munge(s)
+        sagg['iteration'] = i
+        results = pd.concat([sagg, results])
+    return results
 
 
 measures = {
@@ -38,19 +42,17 @@ measures = {
 
 
 def cap_daily_measures(df, measures):
-  for m in measures:
-    col = F.col(m)
-    cap = measures[m]
-    df = df.withColumn(m, F.when(col > cap, cap).otherwise(col))
-  return df
+    for m in measures:
+        col = F.col(m)
+        cap = measures[m]
+        df = df.withColumn(m, F.when(col > cap, cap).otherwise(col))
+    return df
 
 
 def percent_capped(df, measures):
     return(df
-    .agg(
-      *[(F.avg(
-          F.when(F.col(m) == measures[m], 1)
-           .otherwise(0))).alias(m) 
-        for m in measures])
-    ).toPandas().T
-  
+           .agg(*[(F.avg(
+                  F.when(F.col(m) == measures[m], 1)
+                   .otherwise(0))).alias(m)
+                  for m in measures])
+           ).toPandas().T

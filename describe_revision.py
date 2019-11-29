60514d9f93eaa982b674c816e8c959593381e034
Tim Smith <tdsmith@mozilla.com>
2019-11-29 10:12:46 -0800

    Suppress patch (oops)

--- describe_revision.py
+++ describe_revision.py
@@ -5,7 +5,7 @@ import datetime as dt
 import subprocess
 
 ref = subprocess.check_output("git describe --dirty --all --long --first-parent --match master".split()).strip().decode("utf-8")
-last_commit = subprocess.check_output('git show --format=format:%ct master'.split()).strip().decode("ascii")
+last_commit = subprocess.check_output('git show -s --format=format:%ct master'.split()).strip().decode("ascii")
 
 version_string = dt.datetime.utcfromtimestamp(int(last_commit)).strftime("%Y%m%d%H%M%S")
 print(ref.replace("heads/master", version_string))
